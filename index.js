6cd2e57469f70998bd83eaf19d96393bcaf710ac
bhavnesh <bhavneshsharma@loopmethods.com>
2024-12-14 19:14:23 +0530

    minor

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const sumofTwoNumbers = (num1, num2) => {
     return num1 + num2;
 }
 
-module.exports = sumofTwoNumbers;
+const multiplyTwoNumbers = (num1, num2) => {
+    return num1 * num2;
+}
+
+module.exports = {
+    sumofTwoNumbers,
+    multiplyTwoNumbers
+};
